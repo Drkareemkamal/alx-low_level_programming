750ede6d75329f0045b3cf9fb03954593f74433e
kareem kamal <dr.kareeeem@gmail.com>
2023-02-16 07:35:57 -0800

    def

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry pts
- * return : always 0 (sucess)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
