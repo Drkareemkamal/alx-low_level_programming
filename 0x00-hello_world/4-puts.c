8af98a3a07b77756325a74c0a46485f5ae9bae71
kareem kamal <dr.kareeeem@gmail.com>
2023-02-16 06:50:54 -0800

    first c function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - entry point
+ * return : Alaways 0 (success)
+ */
+int main (void)
+{
+	puts("\"Promgramming is like building a multilingual puzzle");
+		return(0);
+}
